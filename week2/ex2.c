eb19491eb0ca06f70623484428c5b82124d144ed
Jud1cator <yarik200027@gmail.com>
2019-08-28 14:05:58 +0300

    Update ex2.c

--- week2/ex2.c
+++ week2/ex2.c
@@ -4,17 +4,17 @@
 
 int main()
 {
-char *s, c;
-int n = 0;
-do {
-	c = getchar();
-	n++;
-	s = realloc(s, n * sizeof(char));
-	s[n-1] = c;
-} while(c != '\n');
-for(int i = strlen(s)-1; i >= 0; i--)
-{
-	printf("%c", s[i]);
-}
+	char *s, c;
+	int n = 0;
+	do {
+		c = getchar();
+		n++;
+		s = realloc(s, n * sizeof(char));
+		s[n-1] = c;
+	} while(c != '\n');
+	for(int i = strlen(s)-1; i >= 0; i--)
+	{
+		printf("%c", s[i]);
+	}
 return 0;
 }
