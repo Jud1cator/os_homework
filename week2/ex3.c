6ffb6d56492ad4bda275946ab8fe27b958f88da2
Jud1cator <yarik200027@gmail.com>
2019-08-28 14:07:03 +0300

    Update ex3.c

--- week2/ex3.c
+++ week2/ex3.c
@@ -3,12 +3,12 @@
 
 int main(int argc, char *argv[])
 {
-int n = atoi(argv[1]);
-for(int i = 1; i <= n; i++)
-{
-	for(int j = 0; j < n - i; j++) putchar(' ');
-	for(int j = 0; j < 2*i-1; j++) putchar('*');
-	putchar('\n');
-}
-return 0;
+	int n = atoi(argv[1]);
+	for(int i = 1; i <= n; i++)
+	{
+		for(int j = 0; j < n - i; j++) putchar(' ');
+		for(int j = 0; j < 2*i-1; j++) putchar('*');
+		putchar('\n');
+	}
+	return 0;
 }
