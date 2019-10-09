221f85777dfe9b36331864ee91ee57ec59a9454e
Jud1cator <yarik200027@gmail.com>
2019-10-09 16:09:48 +0300

    lab 8 upd.

--- week8/ex2.c
+++ week8/ex2.c
@@ -6,8 +6,8 @@
 int main() {
     for(int i = 0; i < 10; i++) {
         void *p;
-        p = malloc(10240);
-        p = memset(p, 0, 10240);
+        p = malloc(10*1024*1024);
+        p = memset(p, 0, 10*1024*1024);
         sleep(1);
     }
     return 0;
