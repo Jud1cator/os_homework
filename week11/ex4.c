c50f28699276356f7299a7b5a9ed7df9080ad8c0
Jud1cator <yarik200027@gmail.com>
2019-10-30 18:11:04 +0300

    lab 11 fixed

--- week11/ex4.c
+++ week11/ex4.c
@@ -14,14 +14,14 @@ int main()
     struct stat sb;
 
     if((fd1 = open(fname1, O_RDWR)) == -1) exit(1);
-    if((fd2 = open(fname2, O_WRONLY)) == -1) exit(1);
-    if(fstat(fd1, &sb) == -1) exit(1);
+    if((fd2 = open(fname2, O_CREAT|O_RDWR)) == -1) exit(2);
+    if(fstat(fd1, &sb) == -1) exit(3);
 
-    if(ftruncate(fd2, sb.st_size) == -1) exit(1);
+    if(ftruncate(fd2, sb.st_size) == -1) exit(4);
     
-    if((addr1 = mmap(NULL, sb.st_size, PROT_WRITE, MAP_SHARED, fd1, 0)) == MAP_FAILED) exit(1);
+    if((addr1 = mmap(NULL, sb.st_size, PROT_READ, MAP_SHARED, fd1, 0)) == MAP_FAILED) exit(5);
 
-    if((addr2 = mmap(NULL, sb.st_size, PROT_WRITE, MAP_SHARED, fd2, 0)) == MAP_FAILED) exit(1);
+    if((addr2 = mmap(NULL, sb.st_size, PROT_READ|PROT_WRITE, MAP_SHARED, fd2, 0)) == MAP_FAILED) exit(6);
 
     memcpy(addr2, addr1, sb.st_size);
 
